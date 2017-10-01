9055c63b788d0295e54e8fa36edc39fad8889229
yoneda hayato <yhayato1320@gmail.com>
2017-10-01 21:20:51 +0900

    fix log save path

--- log.py
+++ log.py
@@ -1,14 +1,19 @@
 import logging, os
 import os.path
 
+import setting
+log_save_path = setting.log_save_path
+
 # ログの出力名を設定
 logger = logging.getLogger("stock_patterns")
 # ログレベルの設定
 logger.setLevel(10)
 
 # ログのファイル出力先を設定
-if not os.path.exists("./log"): os.mkdir("./log")
-fh = logging.FileHandler("./log/test.log")
+#if not os.path.exists("./log"): os.mkdir("./log")
+if not os.path.exists(log_save_path): os.mkdir(log_save_path)
+
+fh = logging.FileHandler(log_save_path + "/test.log")
 logger.addHandler(fh)
 
 # ログのコンソール出力の設定
